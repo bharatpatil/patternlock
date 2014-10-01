891ad91a9ead5daa95245708a9b6c80200753ec4
Bharat Patil <bharat.patil@airtightnetworks.com>
2014-10-01 09:31:45 +0530

    1. Formatting 2. added support for circle or square

--- patternLock.js
+++ patternLock.js
@@ -1,153 +1,166 @@
 (function($) {
-  'use strict';
-  $.fn.patternLock = function(options) {
+    'use strict';
+    $.fn.patternLock = function(options) {
+        var started = false,
+            nums = [],
+            arrCoordinates = [],
+            that = this;
+        var canvas, canvasContext, context;
+        var i, j, len;
+        var defaults = function() {
+            return {
+                rows: 3,
+                columns: 3,
+                width: 250,
+                height: 250,
+                randomizeIds: false, // this should be used to randomizeId of td
+                isCircle: true // this will be required to identify if holes are of shape circle or square
+            };
+        };
+        //this is to keep from overriding our "defaults" object.
+        var opts = $.extend({}, defaults(), options);
+        var content = '<div class="patternlock" style="width:' + opts.width + 'px;height:' + opts.height + 'px"><div class="insideWrapper"><canvas class="patternLockCanvas" width="100%" height="100%;"></canvas><table class="tbl tbl1" cellspacing="25px">';
+        for (i = 1; i <= opts.rows; i++) {
+            content = content + "<tr>";
+            for (j = 1; j <= opts.columns; j++) {
+                content = content + '<td data-value="' + (i * j) + '">&nbsp;</td>';
+            }
+            content = content + "</tr>";
+        }
+        content = content + '</table></div></div>';
+        this.append(content);
+        canvas = $('.patternLockCanvas', that)[0];
+        canvas.width = opts.width;
+        canvas.height = opts.width;
+        canvasContext = canvas.getContext('2d');
 
-    var started = false, nums = [], arrCoordinates = [], that = this;
-    var canvas, canvasContext, context;
-    var i, j, len;
+        function getMousePos(canvas, evt) {
+            var rect = canvas.getBoundingClientRect();
+            return {
+                x: evt.clientX - rect.left,
+                y: evt.clientY - rect.top
+            };
+        }
 
-    var defaults = function() {
-      return {
-        rows: 3,
-        columns: 3,
-        width: 250,
-        height: 250,
-        randomizeIds: false // this should be used to randomizeId of td
-      };
+        function SimpleCircle(x, y, r) {
+            this.centerX = x;
+            this.centerY = y;
+            this.radius = r;
+        }
+        SimpleCircle.prototype = {
+            distanceTo: function(pageX, pageY) {
+                return Math.sqrt(Math.pow(pageX - this.centerX, 2) + Math.pow(pageY - this.centerY, 2));
+            },
+            includesXY: function(x, y) {
+                return this.distanceTo(x, y) <= this.radius;
+            }
+        };
+
+        function isMouseOverLockHoles(element, left, top) {
+            var offset = element.offset();
+            if (opts.isCircle === true) {
+                var radius = element.width() / 2,
+                    circle = new SimpleCircle(offset.left + radius, offset.top + radius, radius);
+                //TO-DO: are we going to support only circle of square as well
+                return circle.includesXY(left, top);
+            } else {
+                return top >= offset.top && left >= offset.left && left <= (offset.left + element[0].offsetWidth) && top <= (offset.top + element[0].offsetHeight);
+            }
+        }
+
+        function getCenter(ele) {
+            var offset = $(ele).offset(),
+                width = $(ele).outerWidth(),
+                height = $(ele).outerHeight(),
+                centerX = offset.left + width / 2,
+                centerY = offset.top + height / 2,
+                rect = canvas.getBoundingClientRect();
+            centerX = centerX - rect.left;
+            centerY = centerY - rect.top;
+            return {
+                'x': centerX,
+                'y': centerY
+            };
+        }
+
+        function clearCanvas() {
+            canvasContext.clearRect(0, 0, canvas.width, canvas.height);
+            var w = canvas.width;
+            canvas.width = 1;
+            canvas.width = w;
+        }
+
+        function drawLines() {
+            clearCanvas();
+            if (arrCoordinates.length < 2) {
+                return;
+            }
+            var c = arrCoordinates;
+            canvasContext.lineWidth = 4;
+            for (i = 1, len = c.length; i < len; i++) {
+                canvasContext.beginPath();
+                canvasContext.moveTo(c[i - 1].x, c[i - 1].y);
+                canvasContext.lineTo(c[i].x, c[i].y);
+                canvasContext.strokeStyle = '#000000';
+                canvasContext.stroke();
+                canvasContext.closePath();
+            }
+        }
+        $('.tbl').on('vmousemove', function(evt) {
+            evt.preventDefault();
+            context = $(this);
+            $('td', context).each(function() {
+                if (isMouseOverLockHoles($(this), evt.pageX, evt.pageY)) {
+                    var num = $(this).attr('data-value'),
+                        lastNum = nums[nums.length - 1];
+                    if (started === true && lastNum !== num && nums.indexOf(num) === -1) {
+                        arrCoordinates.push(getCenter(this));
+                        $(this).addClass('selected');
+                        nums.push($(this).attr('data-value'));
+                    }
+                }
+            });
+        });
+        $('.tbl').on('vmouseup', function(evt) {
+            evt.preventDefault();
+            context = $(this);
+            $('td', context).each(function() {
+                if (isMouseOverLockHoles($(this), evt.pageX, evt.pageY)) {
+                    $('#pattern').text(nums.join(','));
+                    started = false;
+                    drawLines();
+                }
+            });
+        });
+        $(document).on('vmouseup', function() {
+            if (started === true) {
+                $('#pattern').text(nums.join(','));
+                started = false;
+                drawLines();
+                window.tmo = setTimeout(function() {
+                    $('.tbl td').removeClass('selected');
+                    clearCanvas();
+                }, 1000);
+            }
+        });
+        $('.tbl').on('vmousedown', function(evt) {
+            if (window.tmo) {
+                clearTimeout(window.tmo);
+            }
+            evt.preventDefault();
+            context = $(this);
+            $('td', context).each(function() {
+                if (isMouseOverLockHoles($(this), evt.pageX, evt.pageY)) {
+                    started = true;
+                    nums = [];
+                    arrCoordinates = [];
+                    $('td', context).removeClass('selected');
+                    clearCanvas();
+                    $(this).addClass('selected');
+                    nums.push($(this).attr('data-value'));
+                    arrCoordinates.push(getCenter(this));
+                }
+            });
+        });
     };
-
-    //this is to keep from overriding our "defaults" object.
-    var opts = $.extend({}, defaults(), options);
-
-    var content = '<div class="patternlock" style="width:'+opts.width+'px;height:'+opts.height+'px"><div class="insideWrapper"><canvas class="patternLockCanvas" width="100%" height="100%;"></canvas><table class="tbl tbl1" cellspacing="25px">';
-    for ( i = 1; i <= opts.rows; i++) {
-      content = content + "<tr>";
-      for ( j = 1; j <= opts.columns; j++) {
-        content = content + '<td data-value="' + (i * j) + '">&nbsp;</td>';
-      }
-      content = content + "</tr>";
-    }
-    content = content + '</table></div></div>';
-    this.append(content);
-
-    canvas = $('.patternLockCanvas', that)[0];
-    canvas.width = opts.width;
-    canvas.height = opts.width;
-    canvasContext = canvas.getContext('2d');
-
-    function getMousePos(canvas, evt) {
-      var rect = canvas.getBoundingClientRect();
-      return {
-        x: evt.clientX - rect.left,
-        y: evt.clientY - rect.top
-      };
-    }
-
-    function isMouseOverLockHoles(jqo, left, top) {
-      var d = jqo.offset();
-      return top >= d.top && left >= d.left && left <= (d.left + jqo[0].offsetWidth) && top <= (d.top + jqo[0].offsetHeight);
-    }
-
-    function getCenter(ele) {
-      var offset = $(ele).offset(),
-        width = $(ele).outerWidth(),
-        height = $(ele).outerHeight(),
-        centerX = offset.left + width / 2,
-        centerY = offset.top + height / 2,
-        rect = canvas.getBoundingClientRect();
-      centerX = centerX - rect.left;
-      centerY = centerY - rect.top;
-
-
-      return {
-        'x': centerX,
-        'y': centerY
-      };
-    }
-
-    function clearCanvas() {
-      canvasContext.clearRect(0, 0, canvas.width, canvas.height);
-      var w = canvas.width;
-      canvas.width = 1;
-      canvas.width = w;
-    }
-
-    function drawLines() {
-      clearCanvas();
-      if (arrCoordinates.length < 2){
-              return;
-      }
-      var c = arrCoordinates;
-      canvasContext.lineWidth = 4;
-      for (i = 1, len = c.length; i < len; i++) {
-        canvasContext.beginPath();
-        canvasContext.moveTo(c[i - 1].x, c[i - 1].y);
-        canvasContext.lineTo(c[i].x, c[i].y);
-        canvasContext.strokeStyle = '#000000';
-        canvasContext.stroke();
-        canvasContext.closePath();
-      }
-    }
-
-    $('.tbl').on('vmousemove', function(evt) {
-      evt.preventDefault();
-      context = $(this);
-      $('td', context).each(function() {
-        if (isMouseOverLockHoles($(this), evt.pageX, evt.pageY)) {
-          var num = $(this).attr('data-value'),
-            lastNum = nums[nums.length - 1];
-          if (started === true && lastNum !== num && nums.indexOf(num) === -1) {
-            arrCoordinates.push(getCenter(this));
-            $(this).addClass('selected');
-            nums.push($(this).attr('data-value'));
-          }
-        }
-      });
-    });
-
-    $('.tbl').on('vmouseup', function(evt) {
-      evt.preventDefault();
-      context = $(this);
-      $('td', context).each(function() {
-        if (isMouseOverLockHoles($(this), evt.pageX, evt.pageY)) {
-          $('#pattern').text(nums.join(','));
-          started = false;
-          drawLines();
-        }
-      });
-    });
-
-    $(document).on('vmouseup', function() {
-      if (started === true) {
-        $('#pattern').text(nums.join(','));
-        started = false;
-        drawLines();
-        window.tmo = setTimeout(function() {
-          $('.tbl td').removeClass('selected');
-          clearCanvas();
-        }, 1000);
-      }
-    });
-
-
-    $('.tbl').on('vmousedown', function(evt) {
-      if (window.tmo) {clearTimeout(window.tmo);}
-      evt.preventDefault();
-      context = $(this);
-      $('td', context).each(function() {
-        if (isMouseOverLockHoles($(this), evt.pageX, evt.pageY)) {
-          started = true;
-          nums = [];
-          arrCoordinates = [];
-          $('td', context).removeClass('selected');
-          clearCanvas();
-          $(this).addClass('selected');
-          nums.push($(this).attr('data-value'));
-          arrCoordinates.push(getCenter(this));
-
-        }
-      });
-    });
-
-  };
 })(jQuery);
