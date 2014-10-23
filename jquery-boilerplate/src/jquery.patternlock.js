b65216cd00415b88b6d3b3fb36b3e7bfdf9fc1d6
Bharat Patil <bharatpatil@Bharats-MacBook-Pro-2.local>
2014-10-23 19:20:01 +0530

    added destroy method

--- jquery-boilerplate/src/jquery.patternlock.js
+++ jquery-boilerplate/src/jquery.patternlock.js
@@ -1,6 +1,7 @@
 // the semi-colon before function invocation is a safety net against concatenated
 // scripts and/or other plugins which may not be closed properly.
-;(function($, window, document, undefined) {
+;
+(function($, window, document, undefined) {
     // undefined is used here as the undefined global variable in ECMAScript 3 is
     // mutable (ie. it can be changed by someone else). undefined isn't really being
     // passed in so we can ensure the value of it is truly undefined. In ES5, undefined
@@ -292,10 +293,50 @@
     // A really lightweight plugin wrapper around the constructor,
     // preventing against multiple instantiations
     $.fn[pluginName] = function(options) {
-        return this.each(function() {
-            if (!$.data(this, "plugin_" + pluginName)) {
-                $.data(this, "plugin_" + pluginName, new Plugin(this, options));
-            }
-        });
+        
+        // Is the first parameter an object (options), or was omitted,
+        // instantiate a new instance of the plugin.
+        if (options === undefined || typeof options === 'object') {
+            return this.each(function() {
+                if (!$.data(this, "plugin_" + pluginName)) {
+                    $.data(this, "plugin_" + pluginName, new Plugin(this, options));
+                }
+            });
+        }
+        // If the first parameter is a string and it doesn't start
+        // with an underscore or "contains" the `init`-function,
+        // treat this as a call to a public method.
+        else if (typeof options === 'string' && options[0] !== '_' && options !== 'init') {
+
+            // Cache the method call
+            // to make it possible
+            // to return a value
+            // var returns;
+
+            this.each(function() {
+                // var instance = $.data(this, 'plugin_' + pluginName);
+
+                // Tests that there's already a plugin-instance
+                // and checks that the requested public method exists
+                // if (instance instanceof Plugin && typeof instance[options] === 'function') {
+
+                    // Call the method of our plugin instance,
+                    // and pass it the supplied arguments.
+                    // returns = instance[options].apply(instance, Array.prototype.slice.call(args, 1));
+                // }
+
+                // Allow instances to be destroyed via the 'destroy' method
+                if (options === 'destroy') {
+                    $.data(this, 'plugin_' + pluginName, null);
+                    $(this).empty();
+                }
+            });
+
+            // If the earlier cached method
+            // gives a value back return the value,
+            // otherwise return this to preserve chainability.
+            // return returns !== undefined ? returns : this;
+            return this;
+        }
     };
 })(jQuery, window, document);
