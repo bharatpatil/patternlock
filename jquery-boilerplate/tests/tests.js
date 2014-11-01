93c1bdfa876febb46cbbb71065606053b7b146f8
pagoenka <pa.goenka@rediff.com>
2014-11-01 12:04:50 +0530

    added new test cases for valueseperator,pattern genrated and value array

--- jquery-boilerplate/tests/tests.js
+++ jquery-boilerplate/tests/tests.js
@@ -90,3 +90,46 @@ testOptionGroup1({
   height: 500,
   fieldName: "testHiddenField"
 });
+
+
+function testOptionGroup2(option,cells) {
+
+test('Testing if proper pattern generated based on mouse/touch movement,valueSeparator,valueArray', function() {
+  expect(1);
+  $("#qunit-fixture").patternLock(option);
+  // extending option 
+  option = $.extend({}, testDefaults, option);
+  //Initialising value array 
+  if (option.valueArray.length === 0 || option.valueArray.length !== option.rows * option.columns) {
+                for (i = 0, len = (option.rows * option.columns); i < len; i++) {
+                    option.valueArray[i] = i + 1;
+  }}
+  var fixture = $('#qunit-fixture');
+  var patternExpected = [];
+
+  //Simulation tap and move
+  $("table  tr:nth("+cells[0].r+")  td:nth("+cells[0].c+")").mousedown();
+  patternExpected.push(option.valueArray[cells[0].r*3+cells[0].c]);
+
+  for(var i=1;i < (cells.length);i++){
+    $("table  tr:nth("+cells[i].r+")  td:nth("+cells[i].c+")").mouseenter().mouseleave();
+    patternExpected.push(option.valueArray[cells[i].r*option.rows+cells[i].c]);
+  }
+
+  $("table  tr:nth("+cells[cells.length-1].r+")  td:nth("+cells[cells.length-1].c+")").mouseup();
+  
+
+
+  equal($(fixture).find("input[type=hidden]").val(), patternExpected.join(option.valueSeparator), 'Matching generated pattern :: Expected pattern '+patternExpected.join(option.valueSeparator));
+  $("#qunit-fixture").patternLock('destroy');
+});
+
+}
+
+testOptionGroup2({fieldName: "pattern"},[{r:0,c:0},{r:0,c:1},{r:0,c:2}]); //1,2,3
+testOptionGroup2({fieldName: "pattern"},[{r:0,c:0},{r:1,c:1},{r:1,c:2},{r:2,c:2}]); //1,5,6,9
+testOptionGroup2({fieldName: "pattern"},[{r:0,c:0},{r:1,c:1},{r:1,c:2},{r:2,c:2},{r:2,c:1},{r:2,c:0}]); //1,5,6,9,8,7
+testOptionGroup2({fieldName: "pattern",valueSeparator:"-"},[{r:0,c:0},{r:0,c:1},{r:0,c:2}]); //1-2-3
+testOptionGroup2({fieldName: "pattern",valueArray:['a','b','c','d','e','f','g','h','i']},[{r:0,c:0},{r:0,c:1},{r:0,c:2}]); //a,b,c
+testOptionGroup2({fieldName: "pattern",valueSeparator:"$",valueArray:['a','b','c','d','e','f','g','h','i']},[{r:0,c:0},{r:0,c:1},{r:0,c:2}]); //a$b$c
+testOptionGroup2({fieldName: "pattern",rows:4,columns:4},[{r:0,c:0},{r:1,c:1},{r:1,c:2},{r:2,c:2},{r:3,c:3}]); //1,2,3
