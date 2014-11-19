8f90eedc0152cc50efec23dc8b7440f7788cbdce
Bharat Patil <bharatpatil@Bharats-MacBook-Pro-2.local>
2014-11-19 21:42:45 +0530

    grunt file correction.

--- jquery-patternlock/Gruntfile.js
+++ jquery-patternlock/Gruntfile.js
@@ -3,7 +3,7 @@ module.exports = function(grunt) {
 	grunt.initConfig({
 
 		// Import package manifest
-		pkg: grunt.file.readJSON("boilerplate.jquery.json"),
+		pkg: grunt.file.readJSON("patternlock.jquery.json"),
 
 		// Banner definitions
 		meta: {
